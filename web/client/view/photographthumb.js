3b56bffb4f132914f073c4fb1ca403df4ef1e215
James Goode <james@uwcs.co.uk>
2014-10-21 21:18:39 +0100

    Tidy up thumbnails.

--- web/client/view/photographthumb.js
+++ web/client/view/photographthumb.js
@@ -3,18 +3,19 @@ var StaticView = require('./static').StaticView;
 exports.PhotographThumbView = StaticView.extend(
     {
         template: function() {
-            caption(
-                img(
-                    {
-                        alt: this.model.get('title'),
-                        src: '/jpeg_image?photograph_id=' + this.model.get('photograph_id'),
-                        onclick: (function() {
-                            this.trigger('click');
-                        }).bind(this)
-                    }
-                   )/*,*/
-                /*span(this.model.get('title'))*/
-                );
+            span({ class: 'vertical-align-helper' });
+            img(
+                {
+                    alt: this.model.get('title'),
+                    src:
+                        '/jpeg_image?photograph_id=' +
+                            this.model.get('photograph_id') +
+                            '&height=140&width=140',
+                    onclick: (function() {
+                        this.trigger('click');
+                    }).bind(this)
+                }
+               );
         }
     }
     );
