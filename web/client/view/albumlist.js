bef6269afda1db18746b20aa3abbe71026b12956
James Goode <james@uwcs.co.uk>
2015-05-05 19:13:32 +0100

    Remove old web directory.

--- web/client/view/albumlist.js
+++
@@ -1,8 +0,0 @@
-var GenericListView = require('./genericlist').GenericListView;
-
-exports.AlbumListView = GenericListView.extend(
-    {
-        className: 'pure-menu pure-menu-vertical pure-menu-open albumlist'
-    }
-    );
-
